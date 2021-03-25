2aea5fb48001ea3009f959d54bb1e1bd5d0bf9c8
Christopher Henderson <chris.henderson1999@gmail.com>
2021-03-25 20:42:08 +0000

    Added default gravity code

--- aether/Assets/Scripts/GravityManager.cs
+++ aether/Assets/Scripts/GravityManager.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class GravityManager : MonoBehaviour
 {
     Vector3 gravitySelected;
+Vector3 defaultGravity = new Vector3(0f, -9.81f, 0f);
+
+    void Awake() {
+        setGravity(defaultGravity);
+    }
 
     public void setGravity(Vector3 gravity)
     {
