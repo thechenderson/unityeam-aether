542f76cdc80ddc0eb53177ccd0bb0fba5b91ef98
Christopher Henderson <chris.henderson1999@gmail.com>
2021-03-25 13:06:13 +0000

    Improved gravity update method to work from other scripts. Player jump still broken

--- aether/Assets/Scripts/GravityManager.cs
+++ aether/Assets/Scripts/GravityManager.cs
@@ -6,15 +6,10 @@ public class GravityManager : MonoBehaviour
 {
     public float gravity;
 
-    void Start()
-    {
-        Physics.gravity = new Vector3(0f, -9.8f, 0f);
-    }
-
-    void updateGravity(Vector3 gravity)
+    public void updateGravity(Vector3 gravity)
     {
         Vector3 gravitySelected = gravity;
-        Physics.gravity = new Vector3(0f, -0f, 10f);
+        Physics.gravity = gravitySelected;
         Debug.Log("Gravity Set To: " + gravitySelected);
     }
 }
